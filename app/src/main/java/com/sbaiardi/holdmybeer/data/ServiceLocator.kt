233abda095ed4043f5412be11360efb37bf9f583
Stefano <sbaiardi@bsdsoftware.it>
2021-09-03 10:22:02 +0200

    add Interceptor for retrofit

--- app/src/main/java/com/sbaiardi/holdmybeer/data/ServiceLocator.kt
+++ app/src/main/java/com/sbaiardi/holdmybeer/data/ServiceLocator.kt
@@ -1,16 +1,30 @@
 package com.sbaiardi.holdmybeer.data
 
 import com.sbaiardi.holdmybeer.data.api.BeerApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 object ServiceLocator {
     private const val BASE_URL = ""
 
+    private fun generateOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val httpClientBuilder = OkHttpClient.Builder()
+            .readTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+        return httpClientBuilder.build()
+    }
+
     private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(generateOkHttpClient())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
