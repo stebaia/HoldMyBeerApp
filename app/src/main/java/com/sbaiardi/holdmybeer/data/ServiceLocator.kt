2a80296e79fe17b882f3b6b0a4053d86e0700224
Stefano <sbaiardi@bsdsoftware.it>
2021-09-03 10:22:28 +0200

    set Base url

--- app/src/main/java/com/sbaiardi/holdmybeer/data/ServiceLocator.kt
+++ app/src/main/java/com/sbaiardi/holdmybeer/data/ServiceLocator.kt
@@ -9,7 +9,7 @@ import java.util.*
 import java.util.concurrent.TimeUnit
 
 object ServiceLocator {
-    private const val BASE_URL = ""
+    private const val BASE_URL = "https://api.punkapi.com/v2/"
 
     private fun generateOkHttpClient(): OkHttpClient {
         val loggingInterceptor = HttpLoggingInterceptor()
