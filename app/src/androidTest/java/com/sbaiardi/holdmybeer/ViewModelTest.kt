6798cc770f289b62850a1a3fc8c2bcf8e6910654
stefano <sbaiardi@bsdsoftware.it>
2021-09-04 14:38:11 +0200

    add implementation for test

---
+++ app/src/androidTest/java/com/sbaiardi/holdmybeer/ViewModelTest.kt
@@ -0,0 +1,14 @@
+package com.sbaiardi.holdmybeer
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+}
