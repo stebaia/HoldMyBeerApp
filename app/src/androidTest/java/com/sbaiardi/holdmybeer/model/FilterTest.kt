d6f483fe658c8d499caf5c15ffa9e49ed1c80670
stefano <sbaiardi@bsdsoftware.it>
2021-09-06 11:34:23 +0200

    modify test for dialog

--- app/src/androidTest/java/com/sbaiardi/holdmybeer/model/FilterTest.kt
+++ app/src/androidTest/java/com/sbaiardi/holdmybeer/model/FilterTest.kt
@@ -1,44 +1,46 @@
 package com.sbaiardi.holdmybeer.model
 
+import net.bytebuddy.implementation.bytecode.Throw
 import org.junit.Assert.*
 import org.junit.Test
+import java.lang.NumberFormatException
 
 class FilterTest {
     private val testFilter: Filter = Filter("","")
 
-    @Test
+    @Test(expected = NumberFormatException::class)
     fun testBuildDateErrorTwoZero(){
-        val expected = "Error"
-        assertEquals(expected, Filter.buildDate("00", "0000"))
+        val expected= "error"
+        assertEquals(expected, Filter.buildDate(0,0 ))
     }
 
 
-    @Test
+    @Test(expected = NumberFormatException::class)
     fun testBuildDateErrorOneZeroMonth(){
         val expected = "Error"
-        assertEquals(expected, Filter.buildDate("00", "0001"))
+        assertEquals(expected, Filter.buildDate(0, 1))
     }
-    @Test
+
+    @Test(expected = NumberFormatException::class)
     fun testBuildDateErrorTwoYear(){
         val expected = "Error"
-        assertEquals(expected, Filter.buildDate("00", "0000"))
+        assertEquals(expected, Filter.buildDate(1, 0))
     }
-    @Test
+    @Test(expected = NumberFormatException::class)
     fun testBuildDateErrorLength(){
         val expected = "Error"
-        assertEquals(expected, Filter.buildDate("1", "0001"))
+        assertEquals(expected, Filter.buildDate(1, 1))
     }
 
+    @Test
+    fun testBuildDateCorrectDateOneNumber(){
+        val expected = "01-2000"
+        assertEquals(expected, Filter.buildDate(1, 2000))
+    }
     @Test
     fun testBuildDateCorrectDate(){
-        val expected = "01-2000"
-        assertEquals(expected, Filter.buildDate("01", "2000"))
-    }
-
-    @Test
-    fun testBuildDateCorrectLength(){
-        val expected = "01-0001"
-        assertEquals(expected, Filter.buildDate("01", "0001"))
+        val expected = "11-2000"
+        assertEquals(expected, Filter.buildDate(11, 2000))
     }
 
 }
